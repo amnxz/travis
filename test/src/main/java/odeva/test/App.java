7dbe247aa192771dcf2cbdf31ccd6fe14cd0d552
Ben Salah Amine <bensalah@b11p18.fil.univ-lille1.fr>
2017-09-27 08:48:17 +0200

    ajout methode

--- test/src/main/java/odeva/test/App.java
+++ test/src/main/java/odeva/test/App.java
@@ -4,10 +4,19 @@ package odeva.test;
  * Hello world!
  *
  */
+
+
 public class App 
 {
+	
+	static public String retournerChaine(String xml){
+		String chaine;
+		chaine=xml;
+		return chaine;
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        retournerChaine("testTravis");
     }
 }
